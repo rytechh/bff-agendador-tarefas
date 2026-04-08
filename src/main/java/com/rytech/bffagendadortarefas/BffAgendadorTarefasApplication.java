ef19a07a60d019bc341c57bb9bc67977821de5cf
Raian <raianros15@gmail.com>
2026-04-08 00:20:09 -0300

    configuração do pom_xml e ativação do feign clients

--- src/main/java/com/rytech/bffagendadortarefas/BffAgendadorTarefasApplication.java
+++ src/main/java/com/rytech/bffagendadortarefas/BffAgendadorTarefasApplication.java
@@ -2,8 +2,11 @@ package com.rytech.bffagendadortarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BffAgendadorTarefasApplication {
 
 	public static void main(String[] args) {
